19c1dc19f78fafe69e02bae2bc4c9059a36b7f7b
Ken <kluo.1030@gmail.com>
2025-05-14 23:11:12 -0700

    updates blog progress

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -34,6 +34,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
+    p article_params
     if @article.update(article_params)
       flash[:notice] = "Article was updated successfully"
       redirect_to @article
@@ -48,7 +49,7 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, category_ids: [])
   end
 
   def require_same_user
